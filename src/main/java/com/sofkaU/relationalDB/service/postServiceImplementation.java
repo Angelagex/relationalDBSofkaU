b2c181b41d918335efc79777d8af1d3e4a26470c
Angel Agelvis <agelvisangel@gmail.com>
2022-05-04 03:28:44 -0500

    ADD - Service interface and class added
    The service interfaces declares the methods that we will use later, in the serviceImplementation we inject the repositories and structure the methods we created before

---
+++ src/main/java/com/sofkaU/relationalDB/service/postServiceImplementation.java
@@ -0,0 +1,51 @@
+package com.sofkaU.relationalDB.service;
+
+import com.sofkaU.relationalDB.entity.Comment;
+import com.sofkaU.relationalDB.entity.Post;
+import com.sofkaU.relationalDB.repository.CommentRepository;
+import com.sofkaU.relationalDB.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class postServiceImplementation implements postService{
+
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post createComment(Comment comment) {
+        Post post = postRepository.findById(comment.getFk_post_id()).get();
+        post.addComment(comment);
+        commentRepository.save(comment);
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void deleteComment(Comment comment) {
+       commentRepository.deleteById(comment.getId());
+    }
+
+    @Override
+    public void deletePost(Post post) {
+        Post postToDelete = postRepository.findById(post.getId()).get();
+        if(postToDelete.getComments().size() >= 0 ){
+            postToDelete.getComments().forEach(comment -> commentRepository.deleteById(comment.getId()));
+        }
+        postRepository.deleteById(post.getId());
+    }
+
+    @Override
+    public List<Post> findAllPost() {
+        return postRepository.findAll();
+    }
+}
