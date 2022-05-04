4e98ab49849cc233eb8872977ba9635ee5c8f9af
Angel Agelvis <agelvisangel@gmail.com>
2022-05-04 04:44:14 -0500

    HOTFIX
    Moving the Service annotation, describing the routes of the mapping in the controller

--- src/main/java/com/sofkaU/relationalDB/service/PostServiceImplementation.java
+++ src/main/java/com/sofkaU/relationalDB/service/PostServiceImplementation.java
@@ -5,9 +5,11 @@ import com.sofkaU.relationalDB.entity.Post;
 import com.sofkaU.relationalDB.repository.CommentRepository;
 import com.sofkaU.relationalDB.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PostServiceImplementation implements PostService {
 
 
